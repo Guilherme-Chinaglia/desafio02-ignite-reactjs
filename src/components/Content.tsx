a1cc3cdec060b6265b7736dec5e1f2e47329d343
Guilherme Chinaglia <quinalia.guilherme@gmail.com>
2021-07-23 19:46:39 -0300

    Finished challenge 2

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,48 @@
-export function Content() {
+import React from "react"
+import { MovieCard } from "./MovieCard"
+
+interface ContentProps {
+  movies: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }[],
+  genreResponseProps: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[],
+  selectedGenreId: number
+}
+
+export function Content(props :ContentProps) {
   // Complete aqui
+  return (
+      <div className="container">
+        <header>
+          <span className="category">Categoria: 
+            <span> 
+              {
+                props.genreResponseProps.map(titleCategory => (
+                  titleCategory.id === props.selectedGenreId && titleCategory.title  
+                ))
+              }
+            </span>
+          </span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props.movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+      )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
+import React, { useState } from 'react';
+import { Button } from './Button';
+
+interface genreResponseProps {
+  genreResponseProps: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[],
+  selectedGenreId: number,
+  handleClickButton(id :number): void
+}
+
+export function SideBar(props :genreResponseProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.genreResponseProps.map((genre) => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
